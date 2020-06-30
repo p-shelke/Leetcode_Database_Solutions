01e238fbd8670c7b185da483f36520e12b578798
Prathmesh Shelke <prathamesh2526@gmail.com>
2020-06-29 23:42:37 -0400

    584. Find Customer Referee

---
+++ Easy/584_find_customer_referee.sql
@@ -0,0 +1,8 @@
+584. Find Customer Referee
+https://leetcode.com/problems/find-customer-referee/
+
+
+Write a query to return the list of customers NOT referred by the person with id '2'.
+
+Solution:
+
