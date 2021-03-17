73e92b60f01864f64552031c6adb694498c392b7
Prathmesh Shelke <prathamesh2526@gmail.com>
2021-03-17 00:56:50 -0400

    1050. Actors and Directors Who Cooperated At Least Three Times

--- Easy/1050. Actors and Directors Who Cooperated At Least Three Times.sql
+++ Easy/1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -1,2 +1,12 @@
 1050. Actors and Directors Who Cooperated At Least Three Times
 
+https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+
+Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor have cooperated with the director at least 3 times.
+
+Solution:
+
+SELECT actor_id, director_id 
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) >= 3;
