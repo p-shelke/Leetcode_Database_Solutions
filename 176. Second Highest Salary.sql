85bd0d16dcd79be4d1a104ae46ce6ff35623efe9
Prathmesh Shelke <prathamesh2526@gmail.com>
2021-03-18 20:56:34 -0400

    Update 176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -1,1 +1,15 @@
+Write a SQL query to get the second highest salary from the Employee table.
 
+#Solution 1 
+SELECT MAX(Salary) as SecondHighestSalary 
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee)
+LIMIT 1;
+
+
+
+#Solution 2
+SELECT Salary AS SecondHighestSalary
+FROM EMPLOYEE 
+ORDER BY Salary DESC
+LIMIT 1 OFFSET 1;
